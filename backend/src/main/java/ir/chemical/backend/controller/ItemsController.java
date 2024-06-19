e3b07e9c7000e27a5ad99c5475ef43871d64e236
Radmehr Shiadeh <78790490+Glonki4@users.noreply.github.com>
2024-06-19 18:46:10 +0100

    crud ops, todo: delete

--- backend/src/main/java/ir/chemical/backend/controller/ItemsController.java
+++ backend/src/main/java/ir/chemical/backend/controller/ItemsController.java
@@ -1,15 +1,24 @@
 package ir.chemical.backend.controller;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import ir.chemical.backend.model.Items;
 import ir.chemical.backend.repository.ItemsRepository;
 
 
 @RestController
+@RequestMapping("/api/items")
 public class ItemsController {
     private final ItemsRepository itemsRepository;
 
@@ -17,8 +26,58 @@ public class ItemsController {
         this.itemsRepository = itemsRepository;
     }
 
-    @GetMapping("/api/items")
+    @GetMapping("")
     public List<Items> findAll() {
         return itemsRepository.getAll();
+    }
+
+    @GetMapping("{id}")
+    public Items findById(@PathVariable long id) {
+        Optional<Items> found = itemsRepository.findById(id);
+        if (found.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        } else {
+            return found.get();
+        }
+    }
+
+    @GetMapping("name/{name}")
+    public Items findByName(@PathVariable String name) {
+        Optional<Items> found = itemsRepository.findByName(name);
+        if (found.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        } else {
+            return found.get();
+        }
+    }
+
+    @PostMapping("")
+    public void createNewItem(@RequestBody Items item) {
+        Optional<Items> found = itemsRepository.findById(item.getId());
+        if (found.isEmpty()) {
+            itemsRepository.create(item);
+        } else {
+            throw new ResponseStatusException(HttpStatus.CONFLICT);
+        }
+    }
+
+    @PutMapping("update/{id}")
+    public Items updateByID(@RequestBody Items item, @PathVariable long id) {
+        Optional<Items> found = itemsRepository.findById(id);
+        if (found.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        } else {
+            return itemsRepository.updateById(item, id);
+        }
+    }
+
+    @PutMapping("update/name/{name}")
+    public Items updateByName(@RequestBody Items item, @PathVariable String name) {
+        Optional<Items> found = itemsRepository.findByName(name);
+        if (found.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        } else {
+            return itemsRepository.updateByName(item, name);
+        }
     }
 }
--- backend/src/main/java/ir/chemical/backend/repository/ItemsRepository.java
+++ backend/src/main/java/ir/chemical/backend/repository/ItemsRepository.java
@@ -2,6 +2,7 @@ package ir.chemical.backend.repository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Repository;
 
@@ -20,10 +21,41 @@ public class ItemsRepository {
         return items;
     }
 
+    public Optional<Items> findById(long id) {
+        return items.stream().filter(item -> item.getId() == id).findFirst();
+    }
+
+    public Optional<Items> findByName(String name) {
+        return items.stream().filter(item -> (item.getName() == null ? name == null : item.getName().equals(name))).findFirst();
+    }
+
+    //post
+    public void create(Items item) {
+        items.add(item);
+    }
+
+    public Items updateById(Items item, long id) {
+        Items existingItem = items.stream().filter(i -> i.getId() == id).findFirst().get();
+        existingItem.setName(item.getName());
+        existingItem.setDescription(item.getDescription());
+        existingItem.setType(item.getType());
+        existingItem.setImageURL(item.getImageURL());
+        return existingItem;
+    }
+
+    public Items updateByName(Items item, String name) {
+        Items existingItem = items.stream().filter(i -> (i.getName() == null ? name == null : item.getName().equals(name))).findFirst().get();
+        existingItem.setName(item.getName());
+        existingItem.setDescription(item.getDescription());
+        existingItem.setType(item.getType());
+        existingItem.setImageURL(item.getImageURL());
+        return existingItem;
+    }
+
     @PostConstruct
     public void init() {
         items.add(new Items(1, "n1", "d1", "t1", "i1"));
         items.add(new Items(2, "n2", "d2", "t2", "i2"));
-        items.add(new Items(2, "n3", "d3", "t3", "i3"));
+        items.add(new Items(3, "n3", "d3", "t3", "i3"));
     }
 }
