596d76e5f0f6ae5d0d29c761a666c249b0f63707
Radmehr Shiadeh <78790490+Glonki4@users.noreply.github.com>
2024-06-21 17:01:38 +0100

    schema sql

---
+++ backend/src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS Items (
+    id INT NOT NULL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(255),
+    type VARCHAR(255) NOT NULL,
+    imageURL VARCHAR(255)
+);
