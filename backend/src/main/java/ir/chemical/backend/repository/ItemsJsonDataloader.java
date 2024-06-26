4c4a1e94c7a103c6919c68ed30de505e66617f22
Radmehr Shiadeh <78790490+Glonki4@users.noreply.github.com>
2024-06-26 16:26:39 +0100

    postgres still being a pain in my a

---
+++ backend/src/main/java/ir/chemical/backend/repository/ItemsJsonDataloader.java
@@ -0,0 +1,41 @@
+package ir.chemical.backend.repository;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import aj.org.objectweb.asm.TypeReference;
+import ir.chemical.backend.model.ItemsRecord;
+
+
+@Component
+public class ItemsJsonDataloader implements CommandLineRunner {
+    private static final Logger log = LoggerFactory.getLogger(ItemsJsonDataloader.class);
+    private final ItemsRepository itemsRepository;
+    private final ObjectMapper objectMapper;
+
+    public ItemsJsonDataloader(ObjectMapper objectMapper, ItemsRepository itemsRepository) {
+        this.itemsRepository = itemsRepository;
+        this.objectMapper = objectMapper;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        if (itemsRepository.count() == 0) {
+            try (InputStream inputStream = TypeReference.class.getResourceAsStream("/data/items.json")) {
+                ItemsRecord allItems = objectMapper.readValue(inputStream, ItemsRecord.class);
+                log.info("Reading {} items from items.json", allItems.items().size());
+                itemsRepository.saveAll(allItems.items());                
+            } catch (IOException e) {
+                throw new RuntimeException("Failed to save all items", e);
+            }
+        }
+        }
+        
+    }
